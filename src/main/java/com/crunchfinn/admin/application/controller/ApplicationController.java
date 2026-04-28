447dba1611ec68f0714317ced3a2dc84fcee2ec4
jai0204 <jaideepy02@gmail.com>
2026-04-28 19:12:36 +0530

    add application functionality - create page

--- src/main/java/com/crunchfinn/admin/application/controller/ApplicationController.java
+++ src/main/java/com/crunchfinn/admin/application/controller/ApplicationController.java
@@ -3,11 +3,13 @@ package com.crunchfinn.admin.application.controller;
 import com.crunchfinn.admin.application.dto.*;
 import com.crunchfinn.admin.application.enums.*;
 import com.crunchfinn.admin.application.service.ApplicationService;
+import com.crunchfinn.admin.common.enums.Gender;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -43,4 +45,28 @@ public class ApplicationController {
         return "applications/list";
     }
 
+    @GetMapping("/new")
+    public String showCreateForm(HttpServletRequest request, Model model) {
+        model.addAttribute("application", new CreateApplicationRequest());
+        model.addAttribute("genderList", Gender.values());
+        model.addAttribute("courseLevelList", CourseLevel.values());
+        model.addAttribute("coapplicantNoeList", ApplicantNOE.values());
+        model.addAttribute("collateralCategoryList", CollateralCategory.values());
+        int currentYear = java.time.Year.now().getValue();
+        model.addAttribute("currentYear", currentYear);
+        model.addAttribute("currentPath", request.getRequestURI());
+
+        return "applications/create";
+    }
+
+    // create application
+    @PostMapping
+    public String createApplication(@ModelAttribute CreateApplicationRequest request,
+                                    RedirectAttributes redirectAttributes) {
+        ApplicationResponse response = applicationService.createApplication(request);
+
+        redirectAttributes.addFlashAttribute("success", "Application created successfully.");
+
+        return "redirect:/applications/" + response.getApplicationId();
+    }
 }
